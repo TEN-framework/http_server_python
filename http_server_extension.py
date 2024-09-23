ff0f6467c72a6b33c00bfac0eef161069af9fcd7
Jay Zhang <wangyoucao577@gmail.com>
2024-09-23 02:07:02 +0000

    feat: wait and return result

--- http_server_extension.py
+++ http_server_extension.py
@@ -12,7 +12,7 @@ from functools import partial
 
 
 class HTTPHandler(BaseHTTPRequestHandler):
-    def __init__(self, ten, *args, directory=None, **kwargs):
+    def __init__(self, ten: TenEnv, *args, directory=None, **kwargs):
         logger.info("new handler: %s %s %s", directory, args, kwargs)
         self.ten = ten
         super().__init__(*args, **kwargs)
@@ -24,14 +24,29 @@ class HTTPHandler(BaseHTTPRequestHandler):
                 content_length = int(self.headers["Content-Length"])
                 input = self.rfile.read(content_length).decode("utf-8")
                 logger.info("incoming request %s", input)
-                self.ten.send_cmd(
-                    Cmd.create_from_json(input),
-                    lambda ten, result: logger.info(
-                        "finish send_cmd from http server %s %s", input, result
-                    ),
-                )
-                self.send_response_only(200)
+
+                # processing by send_cmd
+                cmd_result_event = threading.Event()
+                cmd_result: CmdResult
+                def cmd_callback(_, result):
+                    nonlocal cmd_result_event
+                    nonlocal cmd_result
+                    cmd_result = result
+                    logger.info("cmd callback result: {}".format(cmd_result.to_json()))
+                    cmd_result_event.set()
+
+                self.ten.send_cmd(Cmd.create_from_json(input),cmd_callback)
+                event_got = cmd_result_event.wait(timeout=5)
+
+                # return response
+                if not event_got:  # timeout
+                    self.send_response_only(504)
+                    self.end_headers()
+                    return
+                self.send_response(200 if cmd_result.get_status_code() == StatusCode.OK else 502)
+                self.send_header('Content-Type', 'application/json')
                 self.end_headers()
+                self.wfile.write(cmd_result.to_json().encode(encoding='utf_8'))
             except Exception as e:
                 logger.warning("failed to handle request, err {}".format(e))
                 self.send_response_only(500)
