34907b8844ffffda6e3619385cfe7bb73789a464
Jay Zhang <wangyoucao577@gmail.com>
2025-01-01 09:35:26 +0000

    feat: add basic test

---
+++ tests/test_invalid_path.py
@@ -0,0 +1,52 @@
+#
+# Copyright © 2025 Agora
+# This file is part of TEN Framework, an open source project.
+# Licensed under the Apache License, Version 2.0, with certain conditions.
+# Refer to the "LICENSE" file in the root directory for more information.
+#
+from pathlib import Path
+from typing import Optional
+from ten import (
+    ExtensionTester,
+    TenEnvTester,
+    Cmd,
+    CmdResult,
+    StatusCode,
+    TenError,
+)
+import httpx
+
+
+class ExtensionTesterInvalidPathCase1(ExtensionTester):
+    def on_start(self, ten_env: TenEnvTester) -> None:
+        ten_env.on_start_done()
+
+        property_json = {"num": 1, "str": "111"}
+        r = httpx.post("http://127.0.0.1:8888/cmd", json=property_json)
+        print(r)
+        if r.status_code == httpx.codes.NOT_FOUND:
+            ten_env.stop_test()
+
+
+class ExtensionTesterInvalidPathCase2(ExtensionTester):
+    def on_start(self, ten_env: TenEnvTester) -> None:
+        ten_env.on_start_done()
+
+        property_json = {"num": 1, "str": "111"}
+        r = httpx.post("http://127.0.0.1:8888/cmd/aaa/123", json=property_json)
+        print(r)
+        if r.status_code == httpx.codes.NOT_FOUND:
+            ten_env.stop_test()
+
+
+# TODO: disable it currently since second tester seems can not run. enable it once fixed
+# def test_invalid_path():
+#     tester = ExtensionTesterInvalidPathCase1()
+#     tester.add_addon_base_dir(str(Path(__file__).resolve().parent.parent))
+#     tester.set_test_mode_single("http_server_python")
+#     tester.run()
+
+#     tester2 = ExtensionTesterInvalidPathCase2()
+#     tester2.add_addon_base_dir(str(Path(__file__).resolve().parent.parent))
+#     tester2.set_test_mode_single("http_server_python")
+#     tester2.run()
---
+++ tests/test_timeout.py
@@ -0,0 +1,41 @@
+#
+# Copyright © 2025 Agora
+# This file is part of TEN Framework, an open source project.
+# Licensed under the Apache License, Version 2.0, with certain conditions.
+# Refer to the "LICENSE" file in the root directory for more information.
+#
+from pathlib import Path
+from typing import Optional
+from ten import (
+    ExtensionTester,
+    TenEnvTester,
+    Cmd,
+    CmdResult,
+    StatusCode,
+    TenError,
+)
+import httpx
+
+
+class ExtensionTesterTimeout(ExtensionTester):
+    def on_cmd(self, ten_env: TenEnvTester, cmd: Cmd) -> None:
+        print(f"on_cmd name {cmd.get_name()}")
+        # NOTE: DON'T return result so that timeout will occur
+        # ten_env.return_result(CmdResult.create(StatusCode.OK), cmd)
+        pass
+
+    def on_start(self, ten_env: TenEnvTester) -> None:
+        ten_env.on_start_done()
+
+        property_json = {"num": 1, "str": "111"}
+        r = httpx.post("http://127.0.0.1:8888/cmd/abc", json=property_json)
+        print(r)
+        if r.status_code == httpx.codes.GATEWAY_TIMEOUT:
+            ten_env.stop_test()
+
+
+def test_timeout():
+    tester = ExtensionTesterTimeout()
+    tester.add_addon_base_dir(str(Path(__file__).resolve().parent.parent))
+    tester.set_test_mode_single("http_server_python")
+    tester.run()
