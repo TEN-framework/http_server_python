eac1706b6f164827029a1ad5e9f40fb7a6768d83
Ethan Zhang <qianze.zhang@hotmail.com>
2025-11-12 13:19:25 +0000

    fix: fix lint issues

--- http_server_addon.py
+++ http_server_addon.py
@@ -7,7 +7,8 @@ from ten_runtime import (
 
 @register_addon_as_extension("http_server_python")
 class HTTPServerExtensionAddon(Addon):
-    def on_create_instance(self, ten: TenEnv, addon_name: str, context):
+    def on_create_instance(self, ten_env: TenEnv, name: str, context):
         from .http_server_extension import HTTPServerExtension
 
-        ten.on_create_instance_done(HTTPServerExtension(addon_name), context)
+        ten_env.on_create_instance_done(
+            HTTPServerExtension(name), context)
